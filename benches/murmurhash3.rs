72f175f8aeadfe742beb01e0ebc230a6bdf86b44
Richard Zak <richard.j.zak@gmail.com>
2025-05-04 12:15:50 -0400

    chore: satisfy clippy, clippy checks bench in ci
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- benches/murmurhash3.rs
+++ benches/murmurhash3.rs
@@ -8,7 +8,7 @@ use murmurhash3::{murmurhash3_x64_128, murmurhash3_x86_32};
 fn run_bench(c: &mut Criterion, size: u64) {
     c.bench_function("murmurhash 256k", |b| {
         let mut data: Vec<u8> = FromIterator::from_iter((0..size).map(|_| 0u8));
-        rand::thread_rng().fill_bytes(&mut data);
+        rand::rng().fill_bytes(&mut data);
 
         b.iter(|| {
             black_box(murmurhash3_x86_32(&data, 0));
@@ -19,7 +19,7 @@ fn run_bench(c: &mut Criterion, size: u64) {
 fn run_bench128(c: &mut Criterion, size: u64) {
     c.bench_function("murmurhash128 256k", |b| {
         let mut data: Vec<u8> = FromIterator::from_iter((0..size).map(|_| 0u8));
-        rand::thread_rng().fill_bytes(&mut data);
+        rand::rng().fill_bytes(&mut data);
 
         b.iter(|| {
             black_box(murmurhash3_x64_128(&data, 0));
