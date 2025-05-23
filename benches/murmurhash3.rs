d0bc294d533119733affb538a53779d5fa7487a6
Richard Zak <richard.j.zak@gmail.com>
2025-05-22 21:10:52 -0400

    chore: satisfy clippy
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- benches/murmurhash3.rs
+++ benches/murmurhash3.rs
@@ -1,6 +1,7 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use rand::RngCore;
+use std::hint::black_box;
 use std::iter::FromIterator;
 
 use murmurhash3::{murmurhash3_x64_128, murmurhash3_x86_32};
